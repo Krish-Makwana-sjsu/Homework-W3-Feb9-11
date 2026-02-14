8a3f4e5745a5d2a865f8c44d1f394aed97ad6053
Krish Makwana <krish.makwana@sjsu.edu>
2026-02-14 13:01:32 -0800

    maxHeapify & buildMaxHeap added.

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ class WordFreq {
 public class Main {
 
     public static WordFreq[] createWordFreqArray() {
+
         WordFreq[] arr=new WordFreq[10];
 
         arr[0]=new WordFreq();
@@ -45,9 +46,44 @@ public class Main {
         return arr;
     }
 
+    public static void maxHeapify(WordFreq[] arr, int n, int i) {
+
+        int largest=i;
+        int left=2*i+1;
+        int right=2*i+2;
+
+        if(left<n && arr[left].frequency>arr[largest].frequency) {
+            largest=left;
+        }
+
+        if(right<n && arr[right].frequency>arr[largest].frequency) {
+            largest=right;
+        }
+
+        if(largest!=i) {
+            WordFreq temp=arr[i];
+            arr[i]=arr[largest];
+            arr[largest]=temp;
+
+            maxHeapify(arr, n, largest);
+        }
+
+    }
+
+    public static void buildMaxHeap(WordFreq[] arr) {
+
+        int n=arr.length;
+
+        for(int i=(n/2)-1; i>=0; i--) {
+            maxHeapify(arr, n, i);
+        }
+    }
+
     public static void main(String[] args) {
 
         WordFreq[] arr=createWordFreqArray();
+
+        buildMaxHeap(arr);
 
         for(var WordFreq: arr) {
             System.out.println(WordFreq.word+ ": " +WordFreq.frequency);
